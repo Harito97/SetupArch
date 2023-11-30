b00e95edd3a57d76f7222eefd4cbd20538ac1484
Harito97 <harito9703@gmail.com>
2023-12-01 00:45:12 +0700

    update resource

--- Resouces/home/harito/bin/bin/update_resource.sh
+++ Resouces/home/harito/bin/bin/update_resource.sh
@@ -11,24 +11,43 @@ sudo cp update_resource.sh ~/bin/
 # Don't change the code above
 # ---------------------------
 
+### Shell setup
 sudo cp ~/.zshrc ./home/harito/
 # update ~/.zshrc - the setup for my favourite shell
 
+### Grub setup
 sudo cp /etc/default/grub etc/default/grub
 # update config grub
 
+### Login setup
 sudo cp /etc/profile.d/autostart.sh etc/profile.d/autostart.sh
 # update autostart when login script
 
+### Mount setup
 sudo cp /etc/fstab etc/fstab
 # update setup auto mount partitions when boot
 
+### Mirror setup
 sudo cp /etc/pacman.d/mirrorlist etc/pacman.d/mirrorlist
 # config mirror to download need package - some package don't have on other repository
 
+### System service setup
+sudo cp /etc/systemd/system/zookeeper.service etc/systemd/system/zookeeper.service
+# service start zookeeper
+
+sudo cp /etc/systemd/system/kafka.service etc/systemd/system/kafka.service
+# service start kafka
+
+### User command setup 
 sudo cp -rf ~/bin home/harito/bin
 # update folder has my custom command
 
+### Guide for program install by taz, zip, ...
+sudo cp /mnt/DataK/Program/kafka/kafka_setup.txt mnt/DataK/Program/kafka/kafka_setup.txt
+sudo cp /mnt/DataK/Program/xampp/xampp_guide.txt mnt/DataK/Program/xampp/xampp_guide.txt
+sudo cp /mnt/DataK/Program/xampp/xampp_setup.txt mnt/DataK/Program/xampp/xampp_setup.txt
+sudo cp /mnt/DataK/Program/black_hole_wallpaper.png mnt/DataK/Program/black_hole_wallpaper.png
+sudo cp -rf /mnt/DataK/Program/icon/ mnt/DataK/Program/icon/
 
 
 
