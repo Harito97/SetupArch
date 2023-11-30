187eac50ed23139331a61fed3984a10e3a8ad5c0
Harito97 <harito9703@gmail.com>
2023-12-01 00:12:39 +0700

    update package by pacman

--- Guide/script_run/script_install_by_pacman.sh
+++ Guide/script_run/script_install_by_pacman.sh
@@ -6,3 +6,5 @@ sudo pacman -S gdb
 # debug and run program eg: C, C++
 sudo pacman -S libxcb                       
 # package help Qt get xcb library
+sudo pacman -S qt5-connectivity
+# package help connection for kde connect
