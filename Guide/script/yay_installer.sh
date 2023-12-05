882876d608321c47c3c2a6cda7218833e95f60e1
Harito97 <harito9703@gmail.com>
2023-12-05 22:00:42 +0700

    update yay package install

--- Guide/script/yay_installer.sh
+++ Guide/script/yay_installer.sh
@@ -1,4 +1,11 @@
-yay -S drawio-desktop-bin          # draw graph, plan, ...
-yay -S crow-translate              # translate app
-yay -S kdeconnect-git              # kde connect laptop and phone
-yay -S grub-customizer             # custom grub eg: remove entry that don't need
+yay -S drawio-desktop-bin          
+# draw graph, plan, ...
+yay -S crow-translate              
+# translate app
+yay -S kdeconnect-git              
+# kde connect laptop and phone
+yay -S grub-customizer             
+# custom grub eg: remove entry that don't need
+yay -S cpp-httplib
+# cpp library for http request
+
