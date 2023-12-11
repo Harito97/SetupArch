cd1c6d86067b1e308fd8bd87c2b86fe255fc43ab
Harito97 <harito9703@gmail.com>
2023-12-11 13:22:05 +0700

    update script install

--- Resouces/update_resource.sh
+++ Resouces/update_resource.sh
@@ -1,63 +1,62 @@
-# Run this command in /mnt/DataK/Project/SetupArch/Resouces
 # ---------------------------------------------------------
+# Run this command in /mnt/DataK/Project/SetupArch/Resouces
+# IMPORTANCE: NOT RUN THIS COMMAND IN ANY OTHER LOCATION ON ARCH
+# --------------------------------------------------------------
 
 cd /mnt/DataK/Project/SetupArch/Resouces
 # cd correct place 
-
-sudo su
-# cp update_resource.sh ~/bin/
-# update this command in my bin
 
 # ---------------------------
 # Don't change the code above
 # ---------------------------
 
 ### Shell setup
-cp ~/.zshrc ./home/harito/
+sudo cp ~/.zshrc ./home/harito/
 # update ~/.zshrc - the setup for my favourite shell
 
 ### Grub setup
-cp /etc/default/grub etc/default/grub
+sudo cp /etc/default/grub etc/default/grub
 # update config grub
 
 ### Login setup
-cp /etc/profile.d/autostart.sh etc/profile.d/autostart.sh
+sudo cp /etc/profile.d/autostart.sh etc/profile.d/autostart.sh
 # update autostart when login script
 
 ### Mount setup
-cp /etc/fstab etc/fstab
+sudo cp /etc/fstab etc/fstab
 # update setup auto mount partitions when boot
 
 ### Mirror setup
-cp /etc/pacman.d/mirrorlist etc/pacman.d/mirrorlist
+sudo cp /etc/pacman.d/mirrorlist etc/pacman.d/mirrorlist
 # config mirror to download need package - some package don't have on other repository
 
 ### System service setup
-cp /etc/systemd/system/zookeeper.service etc/systemd/system/zookeeper.service
+sudo cp /etc/systemd/system/zookeeper.service etc/systemd/system/zookeeper.service
 # service start zookeeper
 
-cp /etc/systemd/system/kafka.service etc/systemd/system/kafka.service
+sudo cp /etc/systemd/system/kafka.service etc/systemd/system/kafka.service
 # service start kafka
 
 ### Guide for program install by taz, zip, ...
-cp /mnt/DataK/Program/kafka/kafka_setup.txt mnt/DataK/Program/kafka/kafka_setup.txt
-cp /mnt/DataK/Program/xampp/xampp_guide.txt mnt/DataK/Program/xampp/xampp_guide.txt
-cp /mnt/DataK/Program/xampp/xampp_setup.txt mnt/DataK/Program/xampp/xampp_setup.txt
-# cp /mnt/DataK/Program/black_hole_wallpaper.png mnt/DataK/Program/black_hole_wallpaper.png
-# cp -rf /mnt/DataK/Program/icon/ mnt/DataK/Program/icon/
+sudo cp /mnt/DataK/Program/kafka/kafka_setup.txt mnt/DataK/Program/kafka/kafka_setup.txt
+sudo cp /mnt/DataK/Program/xampp/xampp_guide.txt mnt/DataK/Program/xampp/xampp_guide.txt
+sudo cp /mnt/DataK/Program/xampp/xampp_setup.txt mnt/DataK/Program/xampp/xampp_setup.txt
+# sudo cp /mnt/DataK/Program/black_hole_wallpaper.png mnt/DataK/Program/black_hole_wallpaper.png
+# sudo cp -rf /mnt/DataK/Program/icon/ mnt/DataK/Program/icon/
 
 
 
 
 
+
+
+# ---------------------------
+# Don't change the code below
+# ---------------------------
 
 ### User command setup 
-cp -rf ~/bin home/harito/bin
+sudo cp -rf ~/bin home/harito
 # update folder has my custom command
 
-
-### Exit sudo su
-exit
-
-### cd default home
-cd
+echo "Update resouces succesfully"
+# take infor
